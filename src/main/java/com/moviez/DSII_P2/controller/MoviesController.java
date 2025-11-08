e3b643593d3ff9402412bd1d21202f59495341a3
Luiza Menezes <142453208+luizamenezesg@users.noreply.github.com>
2025-11-08 20:36:11 -0300

    Update MoviesController.java
    correção

--- src/main/java/com/moviez/DSII_P2/controller/MoviesController.java
+++ src/main/java/com/moviez/DSII_P2/controller/MoviesController.java
@@ -6,10 +6,22 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class MoviesController {
-  @GetMapping("/movies")
+  @GetMapping({ "/movies", "/movies/list" })
   public String list(Model model) {
     model.addAttribute("pageTitle", "Filmes");
     model.addAttribute("movies", java.util.List.of());
     return "movies/list";
+  }
+
+  @GetMapping("/movies/form")
+  public String form(Model model) {
+    model.addAttribute("pageTitle", "Novo Filme");
+    return "movies/form";
+  }
+
+  @GetMapping("/movies/detail")
+  public String detail(Model model) {
+    model.addAttribute("pageTitle", "Detalhes do Filme");
+    return "movies/detail";
   }
 }
