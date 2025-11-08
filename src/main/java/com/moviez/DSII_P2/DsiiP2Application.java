72a46d103573297c0f9c53119a72eb0518471e1e
heb-k <hebe0465@gmail.com>
2025-11-08 12:59:12 -0300

    Scaffold

---
+++ src/main/java/com/moviez/DSII_P2/DsiiP2Application.java
@@ -0,0 +1,13 @@
+package com.moviez.DSII_P2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DsiiP2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DsiiP2Application.class, args);
+	}
+
+}
